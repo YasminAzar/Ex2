723804976fed6e57e9326c809ae4bb9dee79c480
YasminAzar <44767252+YasminAzar@users.noreply.github.com>
2018-11-25 14:29:47 +0200

    Add files via upload
    Add method for finding azimuth, elevation and distance

--- MyCoords.java
+++ MyCoords.java
@@ -34,7 +34,7 @@ public class MyCoords implements coords_converter {
 	@Override
 	public double distance3d(Point3D gps0, Point3D gps1) {
 		
-		float R=6371000; //Radios of earth in meter
+		double R=6371000; //Radios of earth in meter
 		double Lon_Norm=0.847091174;
 		double disLat=(gps0.x()*Math.PI/180)-(gps1.x()*Math.PI/180);
 		double disLon=(gps0.y()*Math.PI/180)-(gps1.y()*Math.PI/180);
@@ -68,8 +68,29 @@ public class MyCoords implements coords_converter {
 
 	@Override
 	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		double azimuth=0;
+		double diff_Lat=gps1.x()-gps0.x();
+		double diff_Lon=gps1.y()-gps0.y();
+	    azimuth=Math.atan2(diff_Lat,diff_Lon);
+	    azimuth=Math.toDegrees(azimuth);
+	    if(diff_Lat<0) azimuth=180-azimuth;
+		if(diff_Lat<0 && diff_Lon<0) azimuth=180+azimuth;
+		if(diff_Lon<0) azimuth=360-azimuth;
+//	    System.out.println("The azimuth angel is: "+azimuth);
+		
+	    double dz=gps1.z()-gps0.z();
+	    double elevation_in_rad=Math.atan2(distance3d(gps0,gps1), dz);
+	    double elevation_in_deg=Math.toDegrees(elevation_in_rad);
+//	    System.out.println("The elevation angel is: "+elevation_in_deg);
+
+	    double dxy=distance3d(gps0,gps1)*distance3d(gps0,gps1);
+	    double dist=Math.sqrt(dxy*dxy+dz*dz);
+//	    System.out.println("The distance is: "+dist);
+
+		double [] azimuth_elevation_dist= {azimuth,elevation_in_deg,dist};
+
+		return azimuth_elevation_dist;
 	}
 
 	@Override
--- MyCoordsTest.java
+++ MyCoordsTest.java
@@ -19,8 +19,8 @@ public class MyCoordsTest {
 		
 		Point3D New_Point=new Point3D (coo.add(gps, local_vector_in_meter));
 		Point3D New_Point_exepted=new Point3D(32.106352 ,35.205808194261195 ,650.0);
-		//System.out.println(New_Point.toString());
-		//System.out.println(New_Point_exepted.toString());
+		System.out.println(New_Point.toString());
+		System.out.println(New_Point_exepted.toString());
 
 		assertEquals(New_Point.toString(),New_Point_exepted.toString());
 	
@@ -33,9 +33,9 @@ public class MyCoordsTest {
 
 		double dis=coo.distance3d(gps0, gps1);
 		
-		//System.out.println("the dis in meter: "+dis);
-		double exepted= 493.05233;
-		assertEquals(dis,exepted,0.00001);
+		System.out.println("the dis in meter: "+dis);
+		double exepted= 493.052331;
+		assertEquals(dis,exepted,0.000001);
 
 	}
 	
@@ -45,12 +45,28 @@ public class MyCoordsTest {
 		Point3D gps1=new Point3D(32.106352 ,35.205225 ,650.0);
 
 		Point3D Vector3D=coo.vector3D(gps1, gps0);
-		//System.out.println("the Vector3D in meter: "+Vector3D.toString());
+		System.out.println("the Vector3D in meter: "+Vector3D.toString());
 		Point3D expected_point= new Point3D(337.6989920612287,-359.24920673378745,-20.0);
 		assertEquals(Vector3D.toString(),expected_point.toString());
 
 	}
 	
+	@Test
+	public void azimuth_elevation_dist() {
+		Point3D gps0=new Point3D(32.10332,35.20904,670.0);
+		Point3D gps1=new Point3D(32.10635 ,35.20523 ,650.0);
+		double [] azimuth_elevation_dist= coo.azimuth_elevation_dist(gps0, gps1);
+		double [] expected_r= {313.2229,-2.3291,493.0521865};
+		System.out.println("the azimuth_elevation_dist in polar coordinate: ["+azimuth_elevation_dist[0]+", "+azimuth_elevation_dist[1]+", "+azimuth_elevation_dist[2]+"]");
+
+		assertEquals(azimuth_elevation_dist.toString().valueOf(0),expected_r.toString().valueOf(0));
+		assertEquals(azimuth_elevation_dist.toString().valueOf(1),expected_r.toString().valueOf(1));
+		assertEquals(azimuth_elevation_dist.toString().valueOf(2),expected_r.toString().valueOf(2));
+
+
+	}
+	
+
 	@Test
 	public void isValid_GPS_Point() {
 		Point3D gps0=new Point3D(32.103315,35.209039,670.0);
